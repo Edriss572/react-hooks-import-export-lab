8bb878a32ed6f2398f7cd34a771e341d95b0f5a9
Edriss572 <edriss_miakhel@yahoo.com>
2023-06-12 14:57:34 +0300

    The Assigment is done

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,8 @@
-const username = "Liza";
-const city = "New York";
+const user  = {
+  username:'Liza',
+  city: 'New York',
+  image: "https://i.imgur.com/mV8PQxj.gif",
+}
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
-
+export default user
 // export all three variables as named exports
